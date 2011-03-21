efd901d60ca0196bfbde17b2ae8cb80feda5ca85
Viktor Peacock <vikpeacock@gmail.com>
2011-03-21 13:32:40 +0000

    Refactored CSS.

--- vrw_GadgetShop-war/src/java/Controller/Account.java
+++ vrw_GadgetShop-war/src/java/Controller/Account.java
@@ -120,7 +120,9 @@ public class Account extends HttpServlet {
 
             context = new InitialContext();
             customerSessionRemote = (CustomerSessionRemote) context.lookup(
-                    "vrw_GadgetShop/CustomerSession/remote");           
+                    "vrw_GadgetShop/CustomerSession/remote");
+
+            customerSessionRemote.register(customer);
 
         }
         catch(Exception e)
